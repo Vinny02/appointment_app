594de4e4a203ad38e02c33e2c379d3d2daa9a3b4
Vinny02 <vinny.baijoo@gmail.com>
2024-06-27 18:51:19 -0600

    models added

--- patient_management/clinic/models.py
+++ patient_management/clinic/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Clinician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    npi_number = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    location = models.CharField(max_length=100)
 
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Appointment(models.Model):
+    clinician = models.ForeignKey(Clinician, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
+    reason = models.TextField()
+
+    def __str__(self):
+        return f"Appointment on {self.appointment_date} with {self.clinician} for {self.patient}"
